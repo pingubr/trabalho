066b0d74dbe672160c42afc19b7a61ae96a534bf
HectorMouraM <hectormouram@gmail.com>
2018-11-17 02:33:03 -0300

    Continuação da config do Firebase, nada de erros ainda no Android studio, algo de muito errado não está certo //hector

--- Projeto/app/src/main/java/com/example/brunab/projeto/Entidade/Funcionario.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/Entidade/Funcionario.java
@@ -2,33 +2,60 @@ package com.example.brunab.projeto.Entidade;
 
 import java.util.Date;
 
-public class Funcionario extends Pessoa {
+public class Funcionario {
 
+    private String id;
+    private String cpfFunc;
+    private String rgFunc;
+    private String nomeFunc;
+    private String endFunc;
+    private String cargo;
     private Date dataDemissao;
     private Date dataAdmissao;
     private Boolean supervisor;
 
-    public Date getDataDemissao() {
-        return dataDemissao;
+    public Funcionario(){
+        //construtor vazio, exigencia do Firebase
     }
 
-    public void setDataDemissao(Date dataDemissao) {
-        this.dataDemissao = dataDemissao;
+    public Funcionario(String id, String cpfFunc, String rgFunc, String nomeFunc, String endFunc, String cargo){
+        this.id = id;
+        this.cpfFunc = cpfFunc;
+        this.rgFunc = rgFunc;
+        this.nomeFunc = nomeFunc;
+        this.endFunc = endFunc;
+        this.cargo = cargo;
+    }
+
+    public String getCpfFunc() {
+        return cpfFunc;
+    }
+
+    public String getRgFunc() {
+        return rgFunc;
+    }
+
+    public String getNomeFunc() {
+        return nomeFunc;
+    }
+
+    public String getEndFunc() {
+        return endFunc;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public Date getDataDemissao() {
+        return dataDemissao;
     }
 
     public Date getDataAdmissao() {
         return dataAdmissao;
     }
 
-    public void setDataAdmissao(Date dataAdmissao) {
-        this.dataAdmissao = dataAdmissao;
-    }
-
     public Boolean getSupervisor() {
         return supervisor;
-    }
-
-    public void setSupervisor(Boolean supervisor) {
-        this.supervisor = supervisor;
     }
 }
--- Projeto/app/src/main/java/com/example/brunab/projeto/Telas/MainActivity.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/Telas/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.brunab.projeto.Telas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.brunab.projeto.Entidade.Funcionario;
 import com.example.brunab.projeto.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference databaseFuncionario;
+        databaseFuncionario=FirebaseDatabase.getInstance().getReference("Funcionarios");
+
+        String id = databaseFuncionario.push().getKey();
+
+        Funcionario funcionario = new Funcionario(id, "63913658300", "299957480", "Hector", "Avenida", "chefe");
+
+        databaseFuncionario.child(id).setValue(funcionario);
     }
 }
